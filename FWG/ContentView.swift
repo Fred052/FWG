97dd3bfc05db21281bff30e49f16b457dd0f4c45
Fred052 <ferid.suleymanzade05@gmail.com>
2024-12-02 20:17:17 +0300

    Initial Commit

---
+++ FWG/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FWG
+//
+//  Created by Ferid Suleymanzade on 02.12.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FWG/FWGApp.swift
@@ -0,0 +1,17 @@
+//
+//  FWGApp.swift
+//  FWG
+//
+//  Created by Ferid Suleymanzade on 02.12.24.
+//
+
+import SwiftUI
+
+@main
+struct FWGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
